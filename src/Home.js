8f7243e89297a17d8fce2fadef841fe83b35a8ad
mkzt <myatthant68@gmail.com>
2025-05-07 17:32:17 +0630

    Commit change

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import PostList from "./PostList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+
+    const {data : posts,isPending,error} = useFetch("http://localhost:8000/posts")
+
+
+    return (  
+
+
+        <div className="page">
+
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading.....</div>}
+            {posts && <PostList posts={posts} title="All posts"/>}
+
+        </div>
+
+
+    );
+}
+ 
+export default Home;
